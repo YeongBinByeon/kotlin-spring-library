cf54ba7dc834707c5045ea4e051907bb4caa5889
yeongbin <yeongbin6483@gmail.com>
2023-01-03 15:29:28 +0900

    도서관리 애플리케이션 리팩토링 준비하기 / 5강. 사칙연산 계산기의 나눗셈 테스트 작성 까지 커밋

---
+++ library-app/src/test/kotlin/com/group/libraryapp/JunitTest.kt
@@ -0,0 +1,15 @@
+package com.group.libraryapp
+
+import org.junit.jupiter.api.Test
+
+class JunitTest {
+    @Test
+    fun test1(){
+        println("테스트 1")
+    }
+
+    @Test
+    fun test2(){
+        println("테스트 2")
+    }
+}
